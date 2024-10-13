c1d4226cd831eab29b8626b563adddd74dafdaae
김태현 <45249430+MozziDog@users.noreply.github.com>
2024-10-13 17:02:23 +0900

    이동 및 엄폐 코드 개선
    이동 시에 시작/도착 딜레이 설정 가능
    엄폐물 점유 개념 조금 추상화
    점프로 극복 불가능한 엄폐물 추가
    Character.Move() 함수 개선

--- Assets/Scripts/Logic/Entities/Obstacle.cs
+++ Assets/Scripts/Logic/Entities/Obstacle.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+/// <summary>
+/// 엄폐물.
+/// 캐릭터가 엄폐하거나 뛰어넘는 중에는 엄폐물을 '점유'한 것으로 취급,
+/// '점유'된 중에는 다른 캐릭터의 길찾기 알고리즘에 경로로 고려되지 않도록 OffMeshLink 비활성화
+/// </summary>
 public class Obstacle : MonoBehaviour 
 {
     public Transform[] CoveringPoint;
-    public bool isOccupied = false;
+    public bool isOccupied {
+        get {
+            return _isOccupied;
+        }
+        set {
+            _isOccupied = value;
+            if(canJumpOver)
+                offMeshLink.activated = !value;
+        }
+    }
+
+    [SerializeField] OffMeshLink offMeshLink;
+    [SerializeField] bool canJumpOver;
+    [SerializeField] bool _isOccupied = false;
 }
