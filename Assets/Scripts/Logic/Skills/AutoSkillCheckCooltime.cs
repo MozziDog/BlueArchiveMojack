c1f38f21f6d3efcab679b18ac0225f60f6cfa7b5
김태현 <45249430+MozziDog@users.noreply.github.com>
2024-10-16 17:28:53 +0900

    일반스킬 사용 조건 추가

---
+++ Assets/Scripts/Logic/Skills/AutoSkillCheckCooltime.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSkillCheckCooltime : IAutoSkillCheck
+{
+    int _cooldownTick;
+    bool _skillAvailable = false;
+    IEnumerator _cooldownCoroutine;
+
+    public AutoSkillCheckCooltime(int cooldownTick)
+    {
+        this._cooldownTick = cooldownTick;
+        _cooldownCoroutine = CooldownCoroutine();
+    }
+
+    public void CheckSkillCondition()
+    {
+        if(_cooldownCoroutine != null)
+        {
+            _cooldownCoroutine.MoveNext();
+        }
+    }
+
+    IEnumerator CooldownCoroutine()
+    {
+        for(int i=0; i<_cooldownTick; i++)
+        {
+            yield return 0;
+        }
+        _skillAvailable = true;
+        _cooldownCoroutine = null;
+    }
+
+    public bool CanUseSkill()
+    {
+        return _skillAvailable;
+    }
+
+    public void ResetSkillCondition()
+    {
+        _cooldownCoroutine = CooldownCoroutine();
+    }
+}
---
+++ Assets/Scripts/Logic/Skills/AutoSkillCondition.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public class AutoSkillCondition
+{
+    public AutoSkillConditionType conditionType;
+    public int argument;
+}
